ae52b11e7ed5ac94a3bc0866f740bfcbce3e2812
Daniel Irvine <daniel@danielirvine.com>
2020-01-18 14:53:17 +0000

    Implement submissions route (with dynamo calls commented out)

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
 from flask import Flask, jsonify, request
+import uuid
+import boto3
+import os
 
 app = Flask(__name__)
+
+#client = boto3.client('dynamodb', region_name="eu-west-2")
+CONTACTS_TABLE = os.environ.get('CONTACTS_TABLE', 'contacts')
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/submissions', methods=['POST'])
+def submit_contact():
+    data = request.json
+
+    for item in ['first_name', 'last_name', 'email', 'message']:
+        if item not in data:
+            return jsonify({
+                    'status': 'error',
+                    'message': 'Missing required {} parameter'.format(item)
+                    }), 400
+
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    message = data.get('message')
+
+    submission_id = uuid.uuid4().hex
+
+#    client.put_item(
+#            TableName=CONTACTS_TABLE,
+#            Item= {
+#                'id': {'S': submission_id},
+#                'first_name': {'S': first_name},
+#                'last_name': {'S': last_name},
+#                'email': {'S': email},
+##                'message': {'S': message}
+#                }
+#            )
+
+    return jsonify({
+        'status': 'success',
+        'submission': {
+            'id': submission_id,
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'message': message
+            }
+        }), 201
 
 if __name__== '__main':
     app.run()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import app
 import unittest
+import json
 
 class TestHelloWrold(unittest.TestCase):
 
@@ -15,6 +16,56 @@ class TestHelloWrold(unittest.TestCase):
         assert req.status_code == 200
         assert req.get_data(as_text=True) == 'Hello, world!'
 
+class TestSubmission(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_contact_submission(self):
+
+        payload = {
+                "first_name": "Test",
+                "last_name": "Person",
+                "email": "me@example.com",
+                "message": "Test message"
+                }
+
+        req = self.app.post(
+                'submissions',
+                data=json.dumps(payload),
+                content_type="application/json")
+
+        assert req.status_code == 201
+
+        resp = json.loads(req.get_data())
+
+        for item in [ 'id', 'first_name', 'last_name', 'email', 'message']:
+            assert item in resp['submission']
+
+    def test_missing_params(self):
+        payload = {
+                "first_name": "Test",
+                "last_name": "Person",
+                "email": "person@example.com"
+                }
+
+        req = self.app.post(
+                '/submissions',
+                data=json.dumps(payload),
+                content_type="application/json"
+                )
+
+        assert req.status_code == 400
+
+        resp = json.loads(req.get_data())
+
+        assert 'status' in resp
+        assert 'message' in resp
+
+        assert resp['message'] == 'Missing required message parameter'
 
 if __name__ == '__main__':
     unittest.main()
